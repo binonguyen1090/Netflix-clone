33524226f5b15b8bf2b1fbacfcc6589d5acd69e2
Bino <binonguyen1090@gmail.com>
2022-03-02 00:16:23 -0600

    added Footer component

---
+++ src/containers/footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import jumboData from "../fixtures/jumbo.json"
+import { Footer } from "../components";
+
+
+export function FooterContainer() {
+  return (
+    <Footer>
+      <Footer.Title>HELLO</Footer.Title>
+      <Footer.Break/>
+      <Footer.Row>
+        <Footer.Column>
+          <Footer.Link>HELLO 1</Footer.Link>
+          <Footer.Link>HELLO 2</Footer.Link>
+          <Footer.Link>HELLO 3</Footer.Link>
+          <Footer.Link>HELLO 4</Footer.Link>
+          <Footer.Link>HELLO 5</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link>HELLO 1</Footer.Link>
+          <Footer.Link>HELLO 2</Footer.Link>
+          <Footer.Link>HELLO 3</Footer.Link>
+          <Footer.Link>HELLO 4</Footer.Link>
+          <Footer.Link>HELLO 5</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link>HELLO 1</Footer.Link>
+          <Footer.Link>HELLO 2</Footer.Link>
+          <Footer.Link>HELLO 3</Footer.Link>
+          <Footer.Link>HELLO 4</Footer.Link>
+          <Footer.Link>HELLO 5</Footer.Link>
+        </Footer.Column>
+      </Footer.Row>
+      <Footer.Break/>
+      <Footer.Text>HELLO 5</Footer.Text>
+
+    </Footer>
+  )
+}
+
