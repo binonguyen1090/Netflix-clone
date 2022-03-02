00f200aad5fea1a13799c76304974dcedead2833
Bino <binonguyen1090@gmail.com>
2022-03-01 22:42:15 -0600

    add global css for background

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-
+import {GlobalStyles} from './global-styles'
+import 'normalize.css'
 ReactDOM.render(
-    <App />,
+    <><GlobalStyles /><App /></>,
   document.getElementById('root')
 );
