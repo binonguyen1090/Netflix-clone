d0849071494fcdebe69ce1417ef9ab6632244b3e
Bino <binonguyen1090@gmail.com>
2022-03-02 23:20:35 -0600

    added Accordion Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import {JumbotronContainer} from "./containers/jumbotron"
 import {FooterContainer} from "./containers/footer"
+import {AccordionContainer} from "./containers/accordion"
 
 
 export default function App() {
   return (
-    <><JumbotronContainer /><FooterContainer /></>
+    <>
+      <JumbotronContainer />
+      <AccordionContainer/>
+      <FooterContainer />
+    </>
   );
 }
 
--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import jumboData from "../fixtures/jumbo.json"
 import { Footer } from "../components";
 
 
