250624b5cf1b627cd258f719be922dc0c936931d
Bino <binonguyen1090@gmail.com>
2022-03-03 22:29:41 -0600

    add Ruote for user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import {JumbotronContainer} from "./containers/jumbotron"
 import {FooterContainer} from "./containers/footer"
 import {AccordionContainer} from "./containers/accordion"
-
+import * as ROUTES from "./constant/routes"
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <AccordionContainer/>
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path="/user">
+        <p>Hello I am a user</p>
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <JumbotronContainer />
+        <AccordionContainer/>
+        <FooterContainer />
+      </Route>
+    </Router>
   );
 }
 
