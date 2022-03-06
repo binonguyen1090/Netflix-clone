70fa6cc73cbc14d8e8be9e95c11f0d98412de4c2
Bino <binonguyen1090@gmail.com>
2022-03-06 17:48:54 -0600

    seed data and create firebaseContext

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import {GlobalStyles} from './global-styles'
+import { firebase } from './lib/firebase.prod' ;
+import { FirebaseContext } from './context/firebase';
 import 'normalize.css'
 ReactDOM.render(
-    <><GlobalStyles /><App /></>,
+    <>
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
+
+    </>,
   document.getElementById('root')
 );
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,21 @@
+// import Firebase from 'firebase/app';
+import Firebase from "firebase/compat/app";
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import { seedDatabase } from '../seed';
+// we need to somehow seed the database
+// we need a config here
+const config = {
+    apiKey: "AIzaSyCSb0tIbGNlYRqnpl4eJZrkIIG6Id5Sc8c",
+    authDomain: "netflix-21f1a.firebaseapp.com",
+    projectId: "netflix-21f1a",
+    storageBucket: "netflix-21f1a.appspot.com",
+    messagingSenderId: "25314928621",
+    appId: "1:25314928621:web:8ca5f9537c9b1a085029e4"
+}; 
+
+const firebase = Firebase.initializeApp(config);
+
+seedDatabase(firebase)
+
+export { firebase };
