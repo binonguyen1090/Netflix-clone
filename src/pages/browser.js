b4fcdff1aaeab2d3bb61ee710a4447bb6d8aa29e
Bino <binonguyen1090@gmail.com>
2022-03-07 22:39:19 -0600

    get data from firebase to build UI Browser

--- src/pages/browser.js
+++ src/pages/browser.js
@@ -1,6 +1,9 @@
 import React from "react";
-
+import { useContent } from "../hooks";
 const Browser = () => {
+    const {series} = useContent('series')
+    const {films} = useContent('films')
+    console.log(films)
     return(
        <p>From Browser</p>
     )
