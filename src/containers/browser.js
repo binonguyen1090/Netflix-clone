bf21a259e4640d6cb2792cc3dd69c47b38a59a10
Bino <binonguyen1090@gmail.com>
2022-03-08 23:45:09 -0600

    add loading when user click on profile image

--- src/containers/browser.js
+++ src/containers/browser.js
@@ -1,12 +1,25 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { SelectProfileContainer } from "./profile";
 import { FirebaseContext } from "../context/firebase";
+import {Loading} from "../components"
 
 export function BrowserContainer(slides) {
+  const [profile, setProfile] = useState({})
+  const [loading, setLoading] = useState(true)
   const {firebase} = useContext(FirebaseContext)
   const user = firebase.auth().currentUser || {}
 
-  return (
-    <SelectProfileContainer user={user}/>
-  )
+  useEffect(() =>{
+    console.log('profole', profile)
+    setTimeout(()=>{
+      setLoading(false)
+    },30000)
+  },[profile.displayName])
+
+  return profile.displayName ? (
+    loading ?  (
+      <Loading src={user.photoURL} />) : <Loading.ReleaseBody />
+    ):(
+      <SelectProfileContainer user={user} setProfile={setProfile}/>
+      )
 }
