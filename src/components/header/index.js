96eb18b9d4111d697f1ce682b2213dff33d8d25d
Bino <binonguyen1090@gmail.com>
2022-03-09 01:00:10 -0600

    style header with profile dropdown

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link as  ReactRouterLink} from "react-router-dom";
-import { Background,Container,Logo, Feature,FeatureCallOut, Text, ButtonLink} from "./styles/header";
+import {Dropdown, Picture, Profile, Background,Container,Logo, Group, Feature, FeatureCallOut, Text, ButtonLink, Link} from "./styles/header";
 
 const Header = ({bg = true, children,  ...restProps})  =>{
     return (
@@ -15,6 +15,31 @@ Header.Feature = function HeaderFeature({children, ...restProps}){
 Header.Text = function HeaderText({children, ...restProps}){
     return (
         <Text {...restProps}>{children}</Text>
+    )
+}
+Header.Profile = function HeaderProfile({children, ...restProps}){
+    return (
+        <Profile {...restProps}>{children}</Profile>
+    )
+}
+Header.Dropdown = function HeaderDropdown({children, ...restProps}){
+    return (
+        <Dropdown {...restProps}>{children}</Dropdown>
+    )
+}
+Header.Picture = function HeaderPicture({src, ...restProps}){
+    return (
+        <Picture { ... restProps} src={`/images/users/${src}.png`} />
+    )
+}
+Header.TextLink = function HeaderTextLink({children, ...restProps}){
+    return (
+        <Link {...restProps}>{children}</Link>
+    )
+}
+Header.Group = function HeaderGroup({children, ...restProps}){
+    return (
+        <Group {...restProps}>{children}</Group>
     )
 }
 Header.FeatureCallOut = function HeaderFeatureCallOut({children, ...restProps}){
--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -98,3 +98,62 @@ export const FeatureCallOut = styled.h2`
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
   margin: 0;
 `;
+
+export const Group = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Picture = styled.button`
+  background: url(${({ src }) => src});
+  background-size: contain;
+  border: 0;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+`;
+export const Dropdown = styled.div`
+  display: none;
+  position: absolute;
+  background-color: black;
+  padding: 10px;
+  width: 100px;
+  top: 32px;
+  right: 10px;
+  ${Group}:last-of-type ${Link} {
+    cursor: pointer;
+  }
+  ${Group} {
+    margin-bottom: 10px;
+    &:last-of-type {
+      margin-bottom: 0;
+    }
+    ${Link} {
+      cursor: pointer;
+    }
+    ${Picture} {
+      cursor: default;
+    }
+  }
+  button {
+    margin-right: 10px;
+  }
+  p {
+    font-size: 12px;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+`;
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  position: relative;
+  button {
+    cursor: pointer;
+  }
+  &:hover > ${Dropdown} {
+    display: flex;
+    flex-direction: column;
+  }
+`;
